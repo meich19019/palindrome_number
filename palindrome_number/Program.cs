b88b03d805ab403f22100b183f21eb92d8e3e9ea
梅智皓 <meich19019@gmail.com>
2022-06-13 19:07:45 +0800

    Initial commit

---
+++ palindrome_number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace palindrome_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsPalindrome(121));
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string xString = x.ToString();
+            char[] xStringArray = xString.ToCharArray();
+            Array.Reverse(xStringArray);
+            if (xString == new string(xStringArray))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
